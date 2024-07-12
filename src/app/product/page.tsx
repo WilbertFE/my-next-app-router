7892783e6c2f773c6a94054fa8c7117e27cdaa48
WilbertFE <wilbertbernadi699@gmail.com>
2024-07-12 18:02:31 +0700

    11-Intercepting-routes

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,4 +1,5 @@
 import { getData } from "@/services/products";
+import Link from "next/link";
 
 type ProductPageProps = { params: { slug: string[] } };
 
@@ -10,37 +11,35 @@ export default async function ProductPage(props: ProductPageProps) {
       {/* <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1> */}
       {products.data.length > 0 &&
         products.data.map((product: any) => (
-          <div
+          <Link
+            href={`/product/detail/${product.id}`}
             key={product.id}
             className="w-full max-w-sm my-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
           >
-            <a href="#">
-              <img
-                className="p-8 rounded-t-lg object-cover h-96 w-full"
-                src={product.image}
-                alt="product image"
-              />
-            </a>
+            <img
+              className="p-8 rounded-t-lg object-cover h-96 w-full"
+              src={product.image}
+              alt="product image"
+            />
+
             <div className="px-5 pb-5">
-              <a href="#">
-                <h5 className="text-xl truncate font-semibold tracking-tight text-gray-900 dark:text-white">
-                  {product.title}
-                </h5>
-              </a>
+              <h5 className="text-xl truncate font-semibold tracking-tight text-gray-900 dark:text-white">
+                {product.title}
+              </h5>
 
               <div className="flex items-center justify-between mt-3">
                 <span className="text-3xl font-bold text-gray-900 dark:text-white">
                   $ {product.price}
                 </span>
-                <a
-                  href="#"
+                <button
+                  type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Add to cart
-                </a>
+                </button>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       {params.slug && (
         <>
