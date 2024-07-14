d82bdb155eca212b732ed88c09da75abf9b3f061
WilbertFE <wilbertbernadi699@gmail.com>
2024-07-14 17:40:11 +0700

    12-firestore

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,17 +1,18 @@
+import { retrieveData, retrieveDataById } from "@/lib/firebase/service";
 import { NextRequest, NextResponse } from "next/server";
 
 const data = [
   {
     id: 1,
     title: "Sepatu Baru",
-    price: 100000,
+    price: 150000,
     image:
       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/6b16e736-a958-4816-89a3-542601b6616f/ja-1-ep-basketball-shoes-XncH16.png",
   },
   {
     id: 2,
     title: "Sepatu Baru Dong",
-    price: 1000000,
+    price: 1750000,
     image:
       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/6b16e736-a958-4816-89a3-542601b6616f/ja-1-ep-basketball-shoes-XncH16.png",
   },
@@ -36,7 +37,7 @@ export async function GET(request: NextRequest) {
   const id = searchParams.get("id");
 
   if (id) {
-    const detailProduct = data.find((item) => item.id === Number(id));
+    const detailProduct = await retrieveDataById("products", id);
     if (detailProduct) {
       return NextResponse.json({
         status: 200,
@@ -51,5 +52,7 @@ export async function GET(request: NextRequest) {
     });
   }
 
-  return NextResponse.json({ status: 200, message: "Success", data });
+  const products = await retrieveData("products");
+
+  return NextResponse.json({ status: 200, message: "Success", data: products });
 }
---
+++ src/lib/firebase/service.ts
@@ -0,0 +1,30 @@
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  getDoc,
+  doc,
+} from "firebase/firestore";
+import app from "./init";
+
+const firestore = getFirestore(app);
+
+export async function retrieveData(collectionName: string) {
+  const snapshot = await getDocs(collection(firestore, collectionName));
+
+  console.log("snapshot : ", snapshot);
+
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return data;
+}
+
+export async function retrieveDataById(collectionName: string, id: string) {
+  const snapshot = await getDoc(doc(firestore, collectionName, id));
+  console.log("byId snapshot : ", snapshot);
+  const data = snapshot.data();
+  return data;
+}
